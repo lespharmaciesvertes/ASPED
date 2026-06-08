35066d88a291bf238f1c5fa8c9e9b765ac827317
Said Abdou <139704410+lespharmaciesvertes@users.noreply.github.com>
2026-06-08 16:42:05 +0300

    Delete apsed-website directory

--- apsed-website/sanity/lib/image.ts
+++
@@ -1,9 +0,0 @@
-import imageUrlBuilder from '@sanity/image-url';
-import type { Image } from 'sanity';
-import { dataset, projectId } from '../env';
-
-const builder = imageUrlBuilder({ projectId, dataset });
-
-export function urlForImage(source: Image) {
-  return builder.image(source).auto('format').fit('max');
-}
