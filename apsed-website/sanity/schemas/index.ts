620115c6453909c0fadffb8e78cde79a473ff0fc
Said Abdou <139704410+lespharmaciesvertes@users.noreply.github.com>
2026-06-08 17:05:16 +0300

    Delete apsed-website directory

--- apsed-website/sanity/schemas/index.ts
+++
@@ -1,6 +0,0 @@
-import { event } from './event';
-import { newsPost } from './newsPost';
-import { partner } from './partner';
-import { galleryAlbum } from './galleryAlbum';
-
-export const schemaTypes = [event, newsPost, partner, galleryAlbum];
